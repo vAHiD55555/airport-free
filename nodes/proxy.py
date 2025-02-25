8d2136e5bf5834dc226523adbdf83f0d246e28df
小基同學 <73387366+xiaoji235@users.noreply.github.com>
2025-02-25 10:42:41 +0800

    Update proxy.py

--- nodes/proxy.py
+++ nodes/proxy.py
@@ -5,24 +5,17 @@ def get_first_url():
     api_url = "https://api.akams.cn/github"
     
     try:
-        # 发送GET请求
         response = requests.get(api_url)
-        # 检查请求是否成功
         response.raise_for_status()
-        
-        # 解析返回的JSON数据
         json_data = response.json()
-        
-        # 检查data字段是否存在且不为空
         if 'data' in json_data and len(json_data['data']) > 0:
-            # 获取第一个元素中的url字段
             first_url = json_data['data'][0]['url']
             print(first_url)
         else:
-            print("httpsa://github.moeyy.xyz")
+            print("https://github.moeyy.xyz") # 防止无法获取proxy的备用措施
     
     except requests.exceptions.RequestException as e:
-        print(f"请求出错: {e}")
+        print(f"https://ghproxy.net") # 防止无法获取proxy的应急措施
 
 if __name__ == "__main__":
     get_first_url()
