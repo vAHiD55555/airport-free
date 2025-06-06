d6f991d14e6136c62d3deb083300fa7a5b871512
小基同學 <73387366+xiaoji235@users.noreply.github.com>
2025-06-06 16:10:40 +0800

    Update naidounode.py

--- nodes/clash/naidounode.py
+++ nodes/clash/naidounode.py
@@ -17,7 +17,7 @@ DAY = f"{day:02d}"
 
 # 构建URL列表
 urls = [
-    f"https://naidounode.cczzuu.top/node/{year}{MONTH}{DAY}-clash.yaml"
+    f"http://naidounode.cczzuu.top/node/{year}{MONTH}{DAY}-clash.yaml"
 ]
 
 # 发起请求并打印结果
